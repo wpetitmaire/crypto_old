1ef9a5d708194c7a6d273d01526af202bcf1c537
Willy PM <w.petitmaire@gmail.com>
2021-06-05 23:18:51 +0200

    Keep calm and commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,8 @@
 
 export const environment = {
    production: false,
-
-  COINBASE_API_KEY: "aaa",
-  COINBASE_SECRET_KEY: "bbb",
-  COINBASE_BASE_URL: "https://api.coinbase.com",
-  COINBASE_API_VERSION: "/v2",
+   COINBASE_API_KEY: "",
+   COINBASE_SECRET_KEY: "",
+   COINBASE_BASE_URL: "",
+   COINBASE_API_VERSION: "",
 };
