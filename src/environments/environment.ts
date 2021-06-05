f81b4facb547529a6d41524c22aa8c597803c7b0
Willy PM <w.petitmaire@gmail.com>
2021-06-05 23:19:46 +0200

    encore un test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 
 export const environment = {
    production: false,
-   COINBASE_API_KEY: "",
-   COINBASE_SECRET_KEY: "",
+   COINBASE_API_KEY: "eeeee",
+   COINBASE_SECRET_KEY: "eddde",
    COINBASE_BASE_URL: "",
    COINBASE_API_VERSION: "",
 };
