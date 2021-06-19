6424ee96b0d8eb5ce4df4f1319d4d52198520e12
Willy PM <w.petitmaire@gmail.com>
2021-06-19 20:07:17 +0200

    raz env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,8 @@
 
 export const environment = {
    production: false,
-
-  COINBASE_API_KEY: "PHDm3mz8ENCZ41C2",
-  COINBASE_SECRET_KEY: "uuX2LXh7VADrJsn1yQOVvTlPLNpm9iE8",
-  COINBASE_BASE_URL: "https://api.coinbase.com",
-  COINBASE_API_VERSION: "/v2",
+   COINBASE_API_KEY: "",
+   COINBASE_SECRET_KEY: "",
+   COINBASE_BASE_URL: "",
+   COINBASE_API_VERSION: "",
 };
