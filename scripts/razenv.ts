4d6c1ce763a52c81d1ab94178501d729723ef270
Willy PM <w.petitmaire@gmail.com>
2021-07-24 22:27:21 +0200

    test

--- scripts/razenv.ts
+++ scripts/razenv.ts
@@ -1,37 +1,37 @@
-let razenv = () => {
-   var { writeFile } = require('fs');
-var { argv } = require('yargs');
+// let razenv = () => {
+//    var { writeFile } = require('fs');
+// var { argv } = require('yargs');
 
-// Récupération de la configuration dans le .env
-const objetDeConfiguration = require('dotenv').config();
+// // Récupération de la configuration dans le .env
+// const objetDeConfiguration = require('dotenv').config();
 
-// Récupération du fichier de configuration 
-const environnement = argv.environment;
-const environnementDeProduction = environnement === 'prod';
-const cheminVersLeFichier = environnementDeProduction
-   ? `./src/environments/environment.prod.ts`
-   : `./src/environments/environment.ts`;
+// // Récupération du fichier de configuration 
+// const environnement = argv.environment;
+// const environnementDeProduction = environnement === 'prod';
+// const cheminVersLeFichier = environnementDeProduction
+//    ? `./src/environments/environment.prod.ts`
+//    : `./src/environments/environment.ts`;
    
-// Construction du nouveau contenu
-const environmentFileContent = `
-export const environment = {
-   production: ${environnementDeProduction},
-   COINBASE_API_KEY: "",
-   COINBASE_SECRET_KEY: "",
-   COINBASE_BASE_URL: "",
-   COINBASE_API_VERSION: "",
-};
-`;
+// // Construction du nouveau contenu
+// const environmentFileContent = `
+// export const environment = {
+//    production: ${environnementDeProduction},
+//    COINBASE_API_KEY: "",
+//    COINBASE_SECRET_KEY: "",
+//    COINBASE_BASE_URL: "",
+//    COINBASE_API_VERSION: "",
+// };
+// `;
 
-// Écriture dans le fichier
-writeFile(cheminVersLeFichier, environmentFileContent, function (err) {
-   if (err) {
-      console.error(err);
-   }
-   console.info(`Environnement ${cheminVersLeFichier} réinitialisé.`);
-});
+// // Écriture dans le fichier
+// writeFile(cheminVersLeFichier, environmentFileContent, function (err) {
+//    if (err) {
+//       console.error(err);
+//    }
+//    console.info(`Environnement ${cheminVersLeFichier} réinitialisé.`);
+// });
 
 
-}
+// }
 
-razenv();
+// razenv();
--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -1,73 +1,73 @@
-let setenv = () => {
-   var { writeFile } = require('fs');
-var { argv } = require('yargs');
+// let setenv = () => {
+//    var { writeFile } = require('fs');
+// var { argv } = require('yargs');
 
-// Récupération de la configuration dans le .env
-const objetDeConfiguration = require('dotenv').config();
+// // Récupération de la configuration dans le .env
+// const objetDeConfiguration = require('dotenv').config();
 
-// Récupération du fichier de configuration 
-const environnement = argv.environment;
-const environnementDeProduction = environnement === 'prod';
-const cheminVersLeFichier = environnementDeProduction
-   ? `./src/environments/environment.prod.ts`
-   : `./src/environments/environment.ts`;
+// // Récupération du fichier de configuration 
+// const environnement = argv.environment;
+// const environnementDeProduction = environnement === 'prod';
+// const cheminVersLeFichier = environnementDeProduction
+//    ? `./src/environments/environment.prod.ts`
+//    : `./src/environments/environment.ts`;
    
-// Construction du nouveau contenu
-// const environmentFileContent = `
+// // Construction du nouveau contenu
+// // const environmentFileContent = `
+// // export const environment = {
+// //    production: ${isProduction},
+// //    COINBASE_API_KEY: "${process.env.COINBASE_API_KEY}",
+// //    COINBASE_SECRET_KEY: "${process.env.COINBASE_SECRET_KEY}",
+// //    COINBASE_BASE_URL: "${process.env.COINBASE_BASE_URL}",
+// //    COINBASE_API_VERSION: "${process.env.COINBASE_API_VERSION}",
+// // };
+// // `;
+
+// let message = "";
+// const contenuDuNouveauFichierDeConfiguration = () => {
+
+//    const dernierParametre = Object.keys(objetDeConfiguration)[Object.keys(objetDeConfiguration).length - 1];
+//    let sortie = `
 // export const environment = {
-//    production: ${isProduction},
-//    COINBASE_API_KEY: "${process.env.COINBASE_API_KEY}",
-//    COINBASE_SECRET_KEY: "${process.env.COINBASE_SECRET_KEY}",
-//    COINBASE_BASE_URL: "${process.env.COINBASE_BASE_URL}",
-//    COINBASE_API_VERSION: "${process.env.COINBASE_API_VERSION}",
-// };
+//    production: ${environnementDeProduction},\n
 // `;
 
-let message = "";
-const contenuDuNouveauFichierDeConfiguration = () => {
+//    for (const [parametre, valeur] of Object.entries(objetDeConfiguration.parsed)) {
 
-   const dernierParametre = Object.keys(objetDeConfiguration)[Object.keys(objetDeConfiguration).length - 1];
-   let sortie = `
-export const environment = {
-   production: ${environnementDeProduction},\n
-`;
+//       message += `${parametre} --> ${valeur}\n`;
 
-   for (const [parametre, valeur] of Object.entries(objetDeConfiguration.parsed)) {
+//       parametre !== dernierParametre 
+//          ? sortie += `  ${parametre}: "${valeur}",\n`
+//          : sortie += `  ${parametre}: "${valeur}"\n`;
+//    };
 
-      message += `${parametre} --> ${valeur}\n`;
+//    sortie += `};`;
 
-      parametre !== dernierParametre 
-         ? sortie += `  ${parametre}: "${valeur}",\n`
-         : sortie += `  ${parametre}: "${valeur}"\n`;
-   };
+//    console.log(sortie);
+//    console.log(message);
 
-   sortie += `};`;
+//    return sortie;
 
-   console.log(sortie);
-   console.log(message);
+//    return `
+//    export const environment = {
+//       production: ${environnementDeProduction},
+//       COINBASE_API_KEY: "${process.env.COINBASE_API_KEY}",
+//       COINBASE_SECRET_KEY: "${process.env.COINBASE_SECRET_KEY}",
+//       COINBASE_BASE_URL: "${process.env.COINBASE_BASE_URL}",
+//       COINBASE_API_VERSION: "${process.env.COINBASE_API_VERSION}",
+//    };
+//    `
+// }
 
-   return sortie;
-
-   return `
-   export const environment = {
-      production: ${environnementDeProduction},
-      COINBASE_API_KEY: "${process.env.COINBASE_API_KEY}",
-      COINBASE_SECRET_KEY: "${process.env.COINBASE_SECRET_KEY}",
-      COINBASE_BASE_URL: "${process.env.COINBASE_BASE_URL}",
-      COINBASE_API_VERSION: "${process.env.COINBASE_API_VERSION}",
-   };
-   `
-}
-
-// Écriture dans le fichier
-writeFile(cheminVersLeFichier, contenuDuNouveauFichierDeConfiguration(), function (err) {
-   if (err) {
-      console.error(err);
-   }
-   console.info(message);
-});
+// // Écriture dans le fichier
+// writeFile(cheminVersLeFichier, contenuDuNouveauFichierDeConfiguration(), function (err) {
+//    if (err) {
+//       console.error(err);
+//    }
+//    console.info(message);
+// });
 
 
-}
+// }
 
-setenv();
+// setenv();
